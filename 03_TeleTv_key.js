cf8c8d30403a6cce951fa1192a105cd4227b7496
ttele2531-max <ttele2531@gmail.com>
2026-03-31 11:19:01 -0400

    Add files via upload

---
+++ 03_TeleTv_key.js
@@ -0,0 +1,7 @@
+var masterKey = {
+  "brand": "TeleTv",
+  "system_version": "2.1.1-GOLD",
+  "license_status": "Active",
+  "handshake_protocol": "TEL-TV-77-SECURE",
+  "access_tier": "Premium_Vault"
+};
